ed4ab632822a59ff69425ea3d14eb7b08b1d9b72
pkoriris <kipkoririsco4@gmail.com>
2025-01-13 15:02:25 +0300

    update config file

--- multi-day-project/src/firebase/firebaseConfig.js
+++ multi-day-project/src/firebase/firebaseConfig.js
@@ -4,12 +4,12 @@ import 'firebase/auth';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyA-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "week3assignment-2f41d.firebaseapp.com",
+  projectId: "week3assignment-2f41d",
+  storageBucket: "week3assignment-2f41d.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdefghijk123456",
 };
 
 // Initialize Firebase
